4a722253939bac55b0e1ff0c73ee5ceae56ea12a
aryMartinez <arym777@gmail.com>
2020-06-05 15:48:31 -0600

    new component parques

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TiendaComponent } from './components/tienda/tienda.component';
+import { ParquesComponent } from './components/parques/parques.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TiendaComponent
+    TiendaComponent,
+    ParquesComponent
   ],
   imports: [
     BrowserModule,
