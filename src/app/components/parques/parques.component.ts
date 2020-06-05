ee2848d3f31343b17e98d9fb4b2e33333f2346e8
aryMartinez <arym777@gmail.com>
2020-06-05 16:49:36 -0600

    creacion de propiedades

--- src/app/components/parques/parques.component.ts
+++ src/app/components/parques/parques.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parques.component.css']
 })
 export class ParquesComponent implements OnInit {
+  public nombre: string;
+  public metros: number
+  public vegetacion: string
+  public abierto: boolean
 
-  constructor() { }
+  constructor() {
+    this.nombre = 'Parque de caballos'
+    this.metros = 456
+    this.vegetacion = 'Alta'
+    this.abierto = true
+   }
 
   ngOnInit(): void {
   }
